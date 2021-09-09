d1ff478333d3fad06618bb3ba6fcdcb89f672b7e
mrque233 <90395541+mrque233@users.noreply.github.com>
2021-09-09 21:10:55 +0800

    Add files via upload

---
+++ shiyan.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+    int main()
+	{
+	int x;
+	float y;
+	printf(" ‰»Îx£∫");
+	scanf("%d",&x);
+	y=pow(x,3.5)+fabs(x-2)+1/x;
+	printf("µ√µΩy=%.3f",y);
+	return 0;
+    }

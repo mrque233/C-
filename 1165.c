4a7bca1edc22327312e1362b42d4c6b6899c78f3
mrque233 <90395541+mrque233@users.noreply.github.com>
2021-11-10 20:31:11 +0800

    Create 1165.c

---
+++ 1165.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+	int a[3][3]= {1,3,6,7,9,11,14,15,17};
+	int sum=0,i,j;
+	for(i=0; i<3; i++)
+		for(j=0; j<3; j++)
+			if(i==j)  sum+=a[i][j];
+	printf("sum=%d\n",sum);
+	return 0;
+}

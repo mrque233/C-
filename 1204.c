0e5ff15e6b57f3c48be14d61874f8d4e1050bbd3
mrque233 <90395541+mrque233@users.noreply.github.com>
2021-11-30 22:03:46 +0800

    Create 1204.c

---
+++ 1204.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define N 10
+
+int main(void)
+
+{
+
+    int a[N],i,*p;
+
+    p=a;                                          /*$ERROR$*/
+
+    for(i=0;i<5;i++)
+
+         scanf("%d",a+i);                      /*$ERROR$*/
+
+    for(i=0;i<5;i++)
+
+         printf("%4d",p[i]);                   /*$ERROR$*/
+
+    return 0;
+
+}

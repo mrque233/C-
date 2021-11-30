e5de948a9be126ed291374c3ace1f01a30efad14
mrque233 <90395541+mrque233@users.noreply.github.com>
2021-11-30 21:55:13 +0800

    Create 1200.c

---
+++ 1200.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,j;
+    char str[5][20],str1[20];
+    for(i=0;i<5;i++)
+        scanf("%s",str[i]);
+    for(i=0;i<4;i++)
+        for(j=i+1;j<5;j++)
+            if(strcmp(str[i],str[j])>0)
+            {
+                strcpy(str1,str[i]);
+                strcpy(str[i],str[j]);
+                strcpy(str[j],str1);
+            }
+    printf("After sorted:\n");
+    for(i=0;i<5;i++)
+        printf("%s\n",str[i]);
+    return 0;
+}

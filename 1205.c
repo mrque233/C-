876a2f476aad0c36c58485017db78d7bf31680d7
mrque233 <90395541+mrque233@users.noreply.github.com>
2021-11-30 21:56:07 +0800

    Create 1205.c

---
+++ 1205.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{ 
+char ch[30]="This is a test of point";
+char *p;
+p=ch;                                    /*$ERROR$*/
+while(*p!='\0')                           /*$ERROR$*/
+putchar(*p++);                            /*$ERROR$*/
+return 0;
+}

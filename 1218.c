675297ffccc62e1f86470194e3a8948f6dee9c02
mrque233 <90395541+mrque233@users.noreply.github.com>
2021-11-30 22:00:54 +0800

    Create 1218.c

---
+++ 1218.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void swap(int *m,int *n)
+{
+	int t;
+	t=*m;
+	*m=*n;
+	*n=t;
+}
+int main()
+{
+	int a,b,c;
+	int *p1, *p2, *p3;
+	scanf("%d %d %d",&a,&b,&c);
+	p1=&a;
+	p2=&b;
+	p3=&c;
+	if(a>b)
+		swap(p1,p2);
+	if(a>c)
+		swap(p1,p3);
+	if(b>c)
+		swap(p2,p3);
+	printf("%d %d %d\n",a,b,c);
+	return 0;
+}

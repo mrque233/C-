469a5aeee29e2509d9fbbc6860e660a8a6108d35
mrque233 <90395541+mrque233@users.noreply.github.com>
2021-12-10 09:27:35 +0800

    Update 1228.c

--- 1228.c
+++ 1228.c
@@ -1,24 +1,27 @@
 #include<stdio.h>
-struct book{
-	char name[100];
-	double price;
-}books[10];
-int main(){
-	int n,i,min,max;	
-	scanf("%d",&n);
-	for(i=0;i<n;i++){   
-	    scanf("\n");
-	    gets(books[i].name);
-		scanf("%lf",&books[i].price);	
-	}
-	min=max=0;
-	for(i=0;i<n;i++){	
-		if(books[max].price<books[i].price)
-		max=i;
-		if(books[min].price>books[i].price)
-		min=i;
-	}
-	printf("%.2f, %s\n",books[max].price,books[max].name);	
-	printf("%.2f, %s",books[min].price,books[min].name);
+struct pay
+{
+	char name[30];
+	float price;
+}book[10];
+int main()
+{
+	int num;
+	scanf("%d\n",&num);
+	int i;
+	int max=0,min=0; 
+	for(i=0;i<num;i++)
+	 {
+	 	gets(book[i].name);
+	 	scanf("%f\n",&book[i].price);
+	 }
+	 for(i=0;i<num;i++)
+     {  
+         if(book[max].price<book[i].price)
+         max=i;
+         if(book[min].price>book[i].price)
+         min=i;
+     }
+   	printf("%.2f, %s\n%.2f, %s",book[max].price,book[max].name,book[min].price,book[min].name);	
 	return 0;
 }
